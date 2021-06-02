8af074b4d5ea04ec1391b126b438522963e1e8b1
Kevin <Ocyplex@wp.pl>
2021-06-02 13:09:20 +0200

    Zadanie 3+4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -23,10 +23,20 @@ public class Human {
 
 
     public  Double getSalary(){
+        System.out.println("Pobrano informacje o wyplacie w " + java.util.Calendar.getInstance().getTime());
+        System.out.println(this.salary);
         return this.salary;
     }
     public void setSalary(Double salary)
-    {
+        {
+        if(salary > 0){
         this.salary = salary;
-    }
-}
+            System.out.println("Nowa wyplata zostala wyslana do systemu ksiegowego");
+            System.out.println("Prosze odebrac aneks do umowy od pani Hani z Kadru");
+            System.out.println("ZUS i US Wiedza o zmianie w wyplacie, prosze nie ukrywac tego");
+        }
+        else
+        {
+            System.out.println("Niemozna przepisac ujemnej wartosci");
+        }
+}}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,32 +17,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Animal dog = new Animal("owczarek");
-	        dog.name ="Szarik";
-
-            Phone iphone = new Phone();
-            iphone.producer = "apple";
-            iphone.model = "10";
-
-
-        /*
-        dog.takeForAWalk();
-        dog.feed();
-        dog.takeForAWalk();
-        dog.takeForAWalk();
-        dog.takeForAWalk();
-        */
 
         Human human = new Human("Edzio","Dawny",50,3000.0);
-
-        Car vwpassat = new Car("Volkswagen","Passat");
-        human.car = vwpassat;
-
-        System.out.println("Samochod od " + human.name +" to " + vwpassat.producer+" " + vwpassat.model);
-
-
-
-
+        human.getSalary();
+        human.setSalary(-200.0);
+        human.setSalary(5000.0);
+        human.getSalary();
 
     }
 
